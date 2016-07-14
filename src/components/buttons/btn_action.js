c7076adb1e4cc5957766c230f9171e8ce294d08d
Shawn Stringfield <shawn.stringfield@gmail.com>
2016-07-13 22:18:10 -0400

    styled button

--- src/components/buttons/btn_action.js
+++ src/components/buttons/btn_action.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const ButtonAction = () => {
   return (
-    <button className="btn btn-primary">^</button>
+    <button className="btn btn-primary btn-action">
+      <i className="material-icons">keyboard_arrow_down</i>
+    </button>
   )
 }
 
