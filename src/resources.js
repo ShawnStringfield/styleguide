74e07217f460a7041da789f0eeb90b142aa354ce
Shawn Stringfield <sstring@aarp.org>
2018-05-10 11:10:13 -0400

    making urls over https

--- src/resources.js
+++ src/resources.js
@@ -1,6 +1,6 @@
-const themovedbBaseUrl = 'http://api.themoviedb.org/3/'
+const themovedbBaseUrl = 'https://api.themoviedb.org/3/'
 const themovedbApiKey = '?api_key=acd114ebbf19bc5e7a316e1e530d7327';
-const baseImageUrl = 'http://image.tmdb.org/t/p/w300';
+const baseImageUrl = 'https://image.tmdb.org/t/p/w300';
 
 export const movieimageurl = baseImageUrl;
 
